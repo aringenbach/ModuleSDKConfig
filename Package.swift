fa0d816aa5d2a856cde617160c6c9ba39352cf5e
aringenbach <arnaudr@element.io>
2023-05-12 16:53:14 +0200

    Init

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ModuleSDKConfig",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "ModuleSDKConfig",
+            targets: ["ModuleSDKConfig"]),
+    ],
+    dependencies: [
+        .package(path: "../../element-x-ios/Modules/ModuleSDK"),
+        .package(path: "../OnboardingTitleProvider"),
+        .package(path: "../RustSDKModule"),
+    ],
+    targets: [
+        .target(
+            name: "ModuleSDKConfig",
+            dependencies: [
+                "ModuleSDK",
+                "OnboardingTitleProvider",
+                "RustSDKModule",
+            ],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ModuleSDKConfigTests",
+            dependencies: ["ModuleSDKConfig"],
+            path: "Tests"
+        ),
+    ]
+)
